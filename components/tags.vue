67341ff4fc42233cd1536484e8ead6b867f2bb3c
Lasse Diercks <github@lassediercks.de>
2020-04-08 21:12:08 +0200

    feat: move to nuxt

---
+++ components/tags.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="tags">
+    <span class="tag" v-for="tag in tags" :key="tag">{{ tag }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: { tags: Array }
+}
+</script>
+
+<style lang="scss">
+@import './variables';
+
+.tags {
+  font-size: 0.8rem;
+  margin: 0 -0.2rem;
+  display: flex;
+  flex-flow: row wrap;
+}
+.tag {
+  padding: 0.3rem 0.4rem;
+  margin: 0.2rem;
+  background: rgba($grey, 0.2);
+}
+</style>
