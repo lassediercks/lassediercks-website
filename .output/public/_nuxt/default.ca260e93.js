505258ae9f12be9112152d5757d43ab5e9300110
Lasse Diercks <github@lassediercks.de>
2023-05-06 19:00:51 +0200

    dont track artifacts

--- .output/public/_nuxt/default.ca260e93.js
+++
@@ -1,1 +0,0 @@
-import{a as t,b as o,m as r,o as a}from"./entry.98d5cb49.js";const n={};function s(e,c){return a(),o("div",null,[r(e.$slots,"default")])}const f=t(n,[["render",s]]);export{f as default};
